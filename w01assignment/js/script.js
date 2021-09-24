aa0dfccbca85c0d203ca5fa0bddc831560ecce88
N. Tanner Smith <smith.tanner2222@gmail.com>
2021-09-24 16:01:45 -0600

    Update homepage

--- w01assignment/js/script.js
+++
@@ -1,6 +0,0 @@
-
-const copyrightyear = document.getElementById("currentdate2");
-copyrightyear.textContent = new Date().getFullYear();
-
-var string = document.getElementById("lastmodified");
-string.textContent = new Date(document.lastModified);
