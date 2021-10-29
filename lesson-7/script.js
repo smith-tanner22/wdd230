36b3884ec1591e9234400bffe70df18cab078436
N. Tanner Smith <smith.tanner2222@gmail.com>
2021-10-28 21:21:10 -0600

    finished week07 lazy load assignment

---
+++ lesson-7/script.js
@@ -0,0 +1,39 @@
+// get all imgs with data-src attribute
+const imagesToLoad = document.querySelectorAll('img[data-src]');
+
+//optional parameters being set for the IntersectionalObserver
+const imgOptions = {
+
+    threshold: 0,
+    rootMargin: "0px 0px 50px 0px"
+
+};
+
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+        image.removeAttribute('data-src');
+    };
+};
+
+// first check to see if intersection observer is supported
+if ('IntersectionObserver' in window) {
+    const imgObserver = new IntersectionObserver((items, observer) => {
+        items.forEach(item => {
+            if (item.isIntersecting) {
+                loadImages(item.target);
+                observer.unobserve(item.target);
+            }
+        });
+    }, imgOptions);
+
+    // loop through each img and check status and load if necessary
+    imagesToLoad.forEach((img) => {
+        imgObserver.observe(img);
+    });
+
+} else { // just load all images if not supported
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+    });
+}
