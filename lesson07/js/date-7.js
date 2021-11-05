b79beef341cf19b0e2529d78eb960bc11876aa25
N. Tanner Smith <smith.tanner2222@gmail.com>
2021-11-04 19:59:52 -0600

    made adjustments to week07 assignment

---
+++ lesson07/js/date-7.js
@@ -0,0 +1,11 @@
+const options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
+};
+
+
+document.querySelector("#currentDate").textContent = date.toLocaleDateString('en-UK', options);
+
+document.querySelector("#prestonCopyrightYear").textContent = date.getFullYear();
--- lesson07/js/gallery.js
+++ lesson07/js/gallery.js
@@ -63,3 +63,36 @@ function getDateLastVisited(daySinceLastVisit) {
 
 getDateLastVisited(daySinceLastVisit)
 setDateLastVisited()
+
+/*
+// other
+const date = new Date(Date.now());
+
+const options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
+};
+
+
+document.querySelector("#currentDate").textContent = date.toLocaleDateString('en-UK', options);
+
+document.querySelector("#prestonCopyrightYear").textContent = date.getFullYear();
+// make pancake banner work
+
+
+if (date.getDay() === 5) {
+    let banner = document.createElement('div');
+    banner.classList.add('friday-banner');
+    banner.textContent = 'Saturday = Preston Pancakes in the Park! 9:00AM Saturday at the city park pavilion.';
+
+    const main = document.querySelector('body');
+    main.prepend(banner);
+} else {
+    bannerClass = document.querySelector('friday-banner');
+    if (bannerClass != null) {
+        banner.remove('friday-banner');
+    }
+}
+*/
