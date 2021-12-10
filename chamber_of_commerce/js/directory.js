fd1b84f19d2b6d550db7ecd8ce6b077b265b5bf1
N. Tanner Smith <smith.tanner2222@gmail.com>
2021-12-09 19:26:46 -0700

    content for chamber of commerce website

---
+++ chamber_of_commerce/js/directory.js
@@ -0,0 +1,38 @@
+const requestURL = 'business.json'
+
+fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        console.table(jsonObject);
+        const businesses = jsonObject['businesses'];
+
+        for (let i = 0; i < businesses.length; i++) {
+            let card = document.createElement('section');
+            let h2 = document.createElement('h2');
+            let b_address = document.createElement('p');
+            let b_phone = document.createElement('p');
+            let b_owner = document.createElement('p');
+            let b_website = document.createElement('p');
+            let ex_image = document.createElement('img');
+
+            h2.textContent = businesses[i].name;
+            b_address.textContent = 'Address: ' + businesses[i].address;
+            b_phone.textContent = 'Phone: ' + businesses[i].phone;
+            b_owner.textContent = 'Owner(s): ' + businesses[i].owners;
+            b_website.textContent = 'Website: ' + businesses[i].website;
+            ex_image.setAttribute('src', businesses[i].logo);
+            ex_image.setAttribute('alt', businesses[i].name + ' Logo');
+
+            card.appendChild(h2);
+            card.appendChild(b_address);
+            card.appendChild(b_phone);
+            card.appendChild(b_owner);
+            card.appendChild(b_website);
+            card.appendChild(ex_image);
+
+            document.querySelector('div.cards').appendChild(card);
+
+        }
+    });
