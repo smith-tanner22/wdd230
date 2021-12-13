627e78643e31dc702c7974fd947889947ce3c8e1
N. Tanner Smith <smith.tanner2222@gmail.com>
2021-12-12 19:34:18 -0700

    added a view more things

--- chamber_of_commerce/js/home.js
+++ chamber_of_commerce/js/home.js
@@ -1,3 +1,10 @@
 function Hide(HideID) {
     HideID.style.display = "none";
 }
+
+
+const copyrightyear = document.getElementById("currentdate2");
+copyrightyear.textContent = new Date().getFullYear();
+
+var string = document.getElementById("lastmodified");
+string.textContent = new Date(document.lastModified);
